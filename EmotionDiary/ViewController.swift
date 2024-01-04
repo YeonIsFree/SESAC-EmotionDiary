609f25e6c3ea30dd20b909b456175b57ec683f77
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2024-01-04 21:44:59 +0900

    Add UserDefault

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -11,28 +11,39 @@ class ViewController: UIViewController {
     
     @IBOutlet var cntButtons: [UILabel]!
     
-    var count = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setInitialNumber()
-    }
-    
-    func getRandomNumber() -> Int {
-        return Int.random(in: 0...9)
-    }
-    
-    func setInitialNumber() {
-        for idx in 0..<count.count {
-            count[idx] = getRandomNumber()
-            cntButtons[idx].text = "\(count[idx])"
-        }
+        setInitialValue()
     }
     
     @IBAction func imageClicked(_ sender: UITapGestureRecognizer) {
         let id = sender.view!.tag
-        count[id] += 1
-        cntButtons[id].text = "\(count[id])"
+        
+        // 1) UserDefault에서 cnt 값 불러오기
+        let cnt = getValue(id)
+        
+        // 2) 위에서 불러온 cnt 값에 +1 하여 UserDefault 에 업데이트
+        let newCnt = cnt + 1
+        updateValue(id, cnt: newCnt)
+        
+        // 3) button 에 표시
+        cntButtons[id].text = "\(getValue(id))"
+    }
+    
+    func setInitialValue() {
+        // 처음 화면에 표시 될 때 UserDefault에 저장된 값을 불러옴
+        // 만약 저장된 값이 없으면 0으로 표기됨
+        for id in 0...8 {
+            cntButtons[id].text = "\(getValue(id))"
+        }
+    }
+    
+    func getValue(_ id: Int) -> Int {
+        return UserDefaults.standard.integer(forKey: "cnt\(id)")
+    }
+    
+    func updateValue(_ id: Int, cnt: Int) {
+        UserDefaults.standard.set(cnt, forKey: "cnt\(id)")
     }
 }
 
